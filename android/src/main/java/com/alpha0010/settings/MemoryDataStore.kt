0f1cd52f1776840e1c2cc2182f40232e5460b16d
Alpha <alpha0010@users.noreply.github.com>
2022-06-15 22:01:36 -0400

    chore(android): remove dead code

--- android/src/main/java/com/alpha0010/settings/MemoryDataStore.kt
+++ android/src/main/java/com/alpha0010/settings/MemoryDataStore.kt
@@ -7,54 +7,9 @@ import com.facebook.react.bridge.WritableMap
 class MemoryDataStore(private val onChange: (data: WritableMap) -> Unit) : PreferenceDataStore() {
   private val booleans = mutableMapOf<String, Boolean>()
 
-  override fun putString(key: String, value: String?) {
-    TODO()
-    onChange(serialize())
-  }
-
-  override fun putStringSet(key: String, values: Set<String>?) {
-    TODO()
-    onChange(serialize())
-  }
-
-  override fun putInt(key: String, value: Int) {
-    TODO()
-    onChange(serialize())
-  }
-
-  override fun putLong(key: String, value: Long) {
-    TODO()
-    onChange(serialize())
-  }
-
-  override fun putFloat(key: String, value: Float) {
-    TODO()
-    onChange(serialize())
-  }
-
   override fun putBoolean(key: String, value: Boolean) {
     booleans[key] = value
     onChange(serialize())
-  }
-
-  override fun getString(key: String, defValue: String?): String? {
-    return super.getString(key, defValue)
-  }
-
-  override fun getStringSet(key: String, defValues: Set<String>?): Set<String>? {
-    return super.getStringSet(key, defValues)
-  }
-
-  override fun getInt(key: String, defValue: Int): Int {
-    return super.getInt(key, defValue)
-  }
-
-  override fun getLong(key: String, defValue: Long): Long {
-    return super.getLong(key, defValue)
-  }
-
-  override fun getFloat(key: String, defValue: Float): Float {
-    return super.getFloat(key, defValue)
   }
 
   override fun getBoolean(key: String, defValue: Boolean): Boolean {

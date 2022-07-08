5ac6260cf4d28a4abfc43159daa9b400b49ccb3b
Alpha <alpha0010@users.noreply.github.com>
2022-07-08 16:32:01 -0400

    test(event): test event dispatch

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,18 @@
-it.todo('write a test');
+import { dispatchEvent, subscribeEvents } from '../event';
+
+it('dispatches events', () => {
+  const listener = jest.fn();
+  const { id, unsubscribe } = subscribeEvents(listener);
+
+  dispatchEvent({ key: 'value1' }, id + 1);
+  expect(listener).not.toHaveBeenCalled();
+
+  dispatchEvent({ key: 'value2' }, id);
+  expect(listener).toHaveBeenCalledTimes(1);
+  expect(listener).toHaveBeenCalledWith({ key: 'value2' });
+
+  listener.mockClear();
+  unsubscribe();
+  dispatchEvent({ key: 'value3' }, id);
+  expect(listener).not.toHaveBeenCalled();
+});
